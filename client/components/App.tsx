01cc09e688b02f46042e46054b8cb66ddcba8977
Manija Wahab <nooryawahab@gmail.com>
2024-08-01 19:18:13 +1200

    Added base files

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,10 @@
-import { useFruits } from '../hooks/useFruits.ts'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const { data } = useFruits()
-
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        <Outlet />
       </div>
     </>
   )
