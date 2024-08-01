c9c33ae8233f850b0230a65295685e9062d5d079
Hope Robertson <hope.robertson.nz@gmail.com>
2024-08-01 14:59:18 +1200

    added models to models folder

---
+++ models/lights.ts
@@ -0,0 +1,6 @@
+export interface Lights {
+  id: number
+  name: string
+  status: boolean
+  price: number
+}
---
+++ models/repairs.ts
@@ -0,0 +1,5 @@
+export interface Repairs {
+  id: number
+  description: string
+  contact_details: string
+}
