60a2f372fd733956291bfe670b35ce11d6dbcff6
Hope Robertson <hope.robertson.nz@gmail.com>
2024-08-02 18:13:38 +1200

    working on getLightsWithCategories function in db/lights.ts

--- server/db/joins.ts
+++ server/db/joins.ts
@@ -1,20 +1,20 @@
-import knex from 'knex'
+// import knex from 'knex'
 
-export async function getLightsWithCategories() {
-  const lightsWithCategories = await knex('lights as l')
-    .innerJoin('categories as c', 'l.category_id', 'c.id')
-    .select(
-      'l.id as light_id',
-      'l.name as light_name',
-      'l.description as light_description',
-      'l.status as light_status',
-      'l.price as light_price',
-      'l.image_url as light_image_url',
-      'c.id as category_id',
-      'c.category as category_name',
-    )
-  return lightsWithCategories
-}
+// export async function getLightsWithCategories() {
+//  const lightsWithCategories = await knex('lights as l')
+//    .innerJoin('categories as c', 'l.category_id', 'c.id')
+//    .select(
+//      'l.id as light_id',
+//      'l.name as light_name',
+//    'l.description as light_description',
+//      'l.status as light_status',
+//      'l.price as light_price',
+//      'l.image_url as light_image_url',
+//      'c.id as category_id',
+//      'c.category as category_name',
+//   )
+//  return lightsWithCategories
+// }
 
 // export async function getRepairsWithCategories() {
 //  const repairsWithCategories = await knex('repairs as r')
--- server/db/lights.ts
+++ server/db/lights.ts
@@ -1,13 +1,20 @@
 import { Lights, LightsID } from '../../models/lights'
 import connection from './connection'
 
+
 export async function all(): Promise<Lights[]> {
   const lights = await connection('lights').select('*')
   return lights as Lights[]
+
 }
+
 
 export async function byID(id: number): Promise<LightsID | undefined> {
   const light = await connection('lights').where({ id }).first()
 
   return light as LightsID | undefined
+}
+
+export async function getLightsWithCategories(): Promise<LightsWithCategory[]> {
+  const
 }
